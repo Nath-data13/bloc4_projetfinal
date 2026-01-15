21d2595dedf7ae26610bfb2d0bdb7f694a095092
Nath-data13 <nathaliedevogelaere@gmail.com>
2026-01-15 17:36:58 +0100

    test_airflow_feu.py dags

---
+++ airflow_feu/dags/test_airflow_feu.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello_feu():
+    print("🔥 Airflow Projet Feu est opérationnel !")
+    # On tente d'écrire un petit fichier pour tester les permissions
+    with open("/opt/airflow/test_ok.txt", "w") as f:
+        f.write("Validation du DAG le " + str(datetime.now()))
+    return "Succès"
+
+with DAG(
+    dag_id='test_simple_airflow_feu',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,  # Lancement manuel uniquement
+    catchup=False,
+    tags=['debug']
+) as dag:
+
+    task_hello = PythonOperator(
+        task_id='dire_hello',
+        python_callable=hello_feu
+    )
